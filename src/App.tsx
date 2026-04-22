c92b9e0427281d938b9d8513ba8a777378f67595
Souvlaki42 <ilias@moulas.dev>
2026-04-22 21:04:09 +0300

    feat: added basic structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
 export default function App() {
-  return <div></div>;
+  return (
+    <main className="bg-[#111111] flex flex-col w-screen h-screen gap-10 py-42 text-amber-50 font-serif text-2xl">
+      <header className="flex justify-center text-4xl font-bold">
+        <h1>Todo app for Apollo</h1>
+      </header>
+      <section>
+        <form className="w-full flex justify-center">
+          <input
+            type="text"
+            name="todo-add"
+            className="outline-4 w-3/4 h-10 outline-blue-500 hover:outline-blue-400 rounded-lg p-4"
+          />
+        </form>
+      </section>
+    </main>
+  );
 }
